81714d0c7688780e490c07f3d42bac0339ba76bd
kcrennan <katherinecrennan@gmail.com>
2026-02-18 17:12:54 -0500

    Update sketch.js

--- ss1/sketch.js
+++ ss1/sketch.js
@@ -1,8 +1,31 @@
+
+/* Kate C
+
+
+*/
+
+
 function setup() {
   createCanvas(500, 500);
-    background(0,200,0);
+    background(0,0,200); //blue bg
 
 }
 
 function draw() {
+  strokeWeight(3);
+  fill(0,0,100);//blue
+  rect(40,40,200,460); // blue outlined rect on left side
+
+  fill(0,0,200);
+  rect(100,430,80,70);
+
+  line(140,430,140,500)
+
+
+  strokeWeight(1);
+  fill(240,240,191); //yellow
+  circle(450,55,80); // moon on top right
+
+  
+
 }
