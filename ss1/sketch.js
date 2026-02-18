ef5eeec64cbcc293f3fea7164e0ce5ccc7f21ed2
kcrennan <katherinecrennan@gmail.com>
2026-02-18 17:36:01 -0500

    Update sketch.js

--- ss1/sketch.js
+++ ss1/sketch.js
@@ -1,6 +1,7 @@
 
 /* Kate C
 
+just a blue building at 
 
 */
 
@@ -13,16 +14,32 @@ function setup() {
 
 function draw() {
   strokeWeight(3);
+  fill(92,169,4);//green    
+  rect(0,400,500,100);//shrubfence thing
+  strokeWeight(5);
   fill(0,0,100);//blue
   rect(40,40,200,460); // blue outlined rect on left side
+  strokeWeight(3);
+  fill(0,0,200); //lighter blue
+  rect(100,430,80,70);//doors
 
-  fill(0,0,200);
-  rect(100,430,80,70);
+  line(140,430,140,500);//line seperating doors
 
-  line(140,430,140,500)
+  fill(255,255,191);//window color
+  rect(80,50,30,50);//window 1R
+  rect(170,50,30,50);//window 1L
+  rect(80,120,30,50);//window 2R
+  rect(170,120,30,50);//window 2L
+  rect(80,190,30,50);// window 3R
+  rect(170,190,30,50);//window 3L
+  rect(80,260,30,50);//window 4R
+  rect(170,260,30,50);//window 4L
+  rect(80,330,30,50);//window 5R
+  rect(170,330,30,50);//window 5L
+  
 
 
-  strokeWeight(1);
+  strokeWeight(3);
   fill(240,240,191); //yellow
   circle(450,55,80); // moon on top right
 
