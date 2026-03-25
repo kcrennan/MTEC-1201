2e4dceaa00fd6ed31d67a7b916466ac0e23b1212
kcrennan <katherinecrennan@gmail.com>
2026-03-25 17:03:28 -0400

    Update sketch.js

--- Midterm_WIP_KateC/sketch.js
+++ Midterm_WIP_KateC/sketch.js
@@ -49,25 +49,22 @@ function draw()
   scene();
 }
 function mouseClicked(){
-if(mouseX>50 && mouseX<450 && mouseY>620 &&mouseY<1020)  {
+if(mouseX>50 && mouseX<450 && mouseY>620 &&mouseY<1020 && scene===mainMenu)  {
    scene = game1;
   }
-else if(mouseX>700 && mouseX<900 && mouseY>620 &&mouseY<1020)
+else if(mouseX>700 && mouseX<900 && mouseY>620 &&mouseY<1020 && scene===mainMenu)
   {
     scene = game2;
   }
-else if(mouseX>1100 && mouseX<1500 && mouseY>360 &&mouseY<480)
+else if(mouseX>1100 && mouseX<1500 && mouseY>360 &&mouseY<480 && scene===mainMenu)
   {
     scene = game3;
   }
-else if(mouseX>1675 && mouseX<1875 && mouseY>900 &&mouseY<1500)
+else if(mouseX>1675 && mouseX<1875 && mouseY>600 &&mouseY<1000 && scene===mainMenu)
   {
      scene = endScene;
   }
-else
-  {
-    scene = mainMenu;
-  }
+
 }
 function mainMenu()
 {
@@ -165,7 +162,7 @@ rect(0,0,width,height);
 //shrubfence thing
   strokeWeight(3);
   fill(92,169,4);//green    
-  rect(0,400,1000,100);
+  rect(0,900,1920,180);
   //moon
   strokeWeight(3);
   fill(240,240,191); //yellow
@@ -173,45 +170,45 @@ rect(0,0,width,height);
 //blue building
   strokeWeight(5);
   fill(0,0,100);//blue
-  rect(40,40,200,460); // blue outlined rect on left side
+  rect(40,540,200,460); // blue outlined rect on left side
   //2nd building
   fill(twoR,twoG,twoB);//newbuilding for ss3
-  rect(320,80,150, 420);
+  rect(320,580,150, 420);
 //blue building doors
   strokeWeight(3);
   fill(0,0,200); //lighter blue
-  rect(100,430,80,70);//doors
-  line(140,430,140,500);//line seperating doors
+  rect(100,930,80,70);//doors
+  line(140,930,140,1000);//line seperating doors
  //2ndbuilding doors
  fill(twoR+30,twoG+30,twoB+30);
- rect(355,430,80,70);
- line(395,430,395,500);
+ rect(355,930,80,70);
+ line(395,930,395,1000);
  //windows blue building
   fill(winR,winG,winB);//window color
-  rect(80,50,30,50);//window 1R
-  rect(170,50,30,50);//window 1L
-  rect(80,120,30,50);//window 2R
-  rect(170,120,30,50);//window 2L
-  rect(80,190,30,50);// window 3R
-  rect(170,190,30,50);//window 3L
-  rect(80,260,30,50);//window 4R
-  rect(170,260,30,50);//window 4L
-  rect(80,330,30,50);//window 5R
-  rect(170,330,30,50);//window 5L
+  rect(80,550,30,50);//window 1R
+  rect(170,550,30,50);//window 1L
+  rect(80,620,30,50);//window 2R
+  rect(170,620,30,50);//window 2L
+  rect(80,690,30,50);// window 3R
+  rect(170,690,30,50);//window 3L
+  rect(80,760,30,50);//window 4R
+  rect(170,760,30,50);//window 4L
+  rect(80,830,30,50);//window 5R
+  rect(170,830,30,50);//window 5L
   //windows building 2 
   fill(255,255,191);
-  rect(340,100,30,50); //1R
-  rect(420,100,30,50); //1L
-  rect(340,170,30,50); //2R 
-  rect(420,170,30,50); //2L
-  rect(340,240,30,50); //3R
-  rect(420,240,30,50); //3L
-  rect(340,310,30,50); //4R
-  rect(420,310,30,50); //4L
+  rect(340,600,30,50); //1R
+  rect(420,600,30,50); //1L
+  rect(340,670,30,50); //2R 
+  rect(420,670,30,50); //2L
+  rect(340,740,30,50); //3R
+  rect(420,740,30,50); //3L
+  rect(340,810,30,50); //4R
+  rect(420,810,30,50); //4L
   
 
   //windows and window RGB if statement
-   if (mouseX>39  && mouseX <241 && mouseY>39 && mouseY<500)//building coords
+   if (mouseX>39  && mouseX <241 && mouseY>549 && mouseY<1001)//building coords
   {
     winR = 0;//if mouse is on building, lights turn off
     winG = 0;
@@ -227,8 +224,6 @@ rect(0,0,width,height);
     scene = mainMenu;
   }
 }
-
-
 function game3()
 {
   fill(255);
